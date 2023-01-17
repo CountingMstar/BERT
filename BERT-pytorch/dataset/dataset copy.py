c08efe532dd3c352e9567160fd096943cf7ce563
CountingMstar <mwh510@naver.com>
2023-01-17 22:47:03 +0900

    copy2

---
+++ BERT-pytorch/dataset/dataset copy.py
@@ -0,0 +1,124 @@
+# Dataset: 데이터의 샘플과 정답을 저장
+from torch.utils.data import Dataset
+import tqdm
+import torch
+import random
+
+class BERTDataset(Dataset):
+    def __init__(self, corpus_path, vocab, seq_len, encoding="utf-8", corpus_lines=None, on_memory=True):
+        self.vocab = vocab
+        self.seq_len = seq_len
+
+        self.on_memory = on_memory
+        self.corpus_lines = corpus_lines
+        self.corpus_path = corpus_path
+        self.encoding = encoding
+
+        with open(corpus_path, "r", encoding=encoding) as f:
+            if self.corpus_lines is None and not on_memory:
+                for _ in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines):
+                    self.corpus_lines += 1
+                
+                if not on_memory:
+                    self.file = open(corpus_path, "r", encoding=encoding)
+                    self.random_file = open(corpus_path, "r", encoding=encoding)
+
+                    for _ in range(random.randint(self.corpus_lines if self.corpus_lines < 1000 else 1000)):
+                        self.random_file.__next__()
+
+    def __len__(self):
+        return self.corpus_lines
+
+    def __getitem__(self, item):
+        # 랜덤으로 뽑은 문장
+        t1, t2, is_next_label = self.random_sent(item)
+        
+        # 위 문장의 단어 인덱스
+        t1_random, t1_label = self.random_word(t1)
+        t2_random, t2_label = self.random_word(t2)
+
+        # 문장의 앞, 뒤에 시작, 끝 토큰 붙이기
+        t1 = [self.vocab.sos_index] + t1_label + [self.vocab.eos_index]
+        t2 = t2_random + [self.vocab.eos_index]
+
+        t1_label = [self.vocab.pad_index] + t1_label + [self.vocab.pad_index]
+        t2_label = t2_label + [self.vocab.pad_index]
+
+        # input의 세그먼트(문장1, 문장2)
+        segment_label = ([1 for _ in range(len(t1))] + [2 for _ in range(len(t2))])[:self.seq_len]
+        # bert input
+        bert_input = (t1 + t2)[:self.seq_len]
+        # ???
+        bert_label = (t1_label + t2_label)[:self.seq_len]
+
+        padding = [self.vocab.pad_index for _ in range(self.seq_len - len(bert_input))]
+        bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
+
+        output = {"bert_input": bert_input,
+                  "bert_label": bert_label,
+                  "segment_label": segment_label,
+                  "is_next": is_next_label}
+
+        return {key: torch.tensor(value) for key, value in output.items()}
+
+    def random_word(self, sentence):
+        tokens = sentence.split()
+        output_label = []
+
+        for i, token in enumerate(tokens):
+            # random.random(): 0 ~ 1사이의 난수 생성
+            prob = random.random()
+            # 15%만 변환
+            if prob < 0.15:
+                prob /= 0.15
+                # 그중 80%는 마스킹
+                if prob < 0.8:
+                    tokens[i] = self.vocab.mask_index
+                # 10%는 다른 임의의 단어로 변환
+                elif prob < 0.9:
+                    tokens[i] = random.randrange(len(self.vocab))
+                # 나머지 10%는 unkown 처리(원래 논문에서는 원래 그대로)
+                else:
+                    tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+
+                output_label.append(self.vocab.stoi.get(token, self.vocab.unk_index))
+
+            # 나머지 85%는 불변환 
+            # ???
+            else:
+                tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
+                output_label.append(0)
+        
+        return tokens, output_label
+
+    def random_sent(self, index):
+        t1, t2 = self.get_corpus_line(index)
+        
+        # 50%는 isnext, 나머지 50%는 notnext
+        # t1, t2는 이어진 문장을 나눈것
+        if random.random() > 0.5:
+            return t1, t2, 1
+        else:
+            return t1, self.get_random_line(), 0
+
+    def get_corpus_line(self, item):
+        if self.on_memory:
+            return self.lines[item][0], self.lines[item][1]
+        else:
+            line = self.file.__next__()
+            if line is None:
+                self.file.close()
+                self.file = open(self.corpus_path, "r", encoding=self.encoding)
+                line = self.file.__next__()
+
+            t1, t2 = line[:-1].split("\t")
+            return t1, t2
+
+    def get_random_line(self):
+        if self.on_memory:
+            return self.lines[random.randrange(len(self.lines))][1]
+        
+        line = self.file.__next__()
+        return line[:-1].split("\t")[1]
+
+        
---
+++ BERT-pytorch/dataset/vocab copy.py
@@ -0,0 +1,164 @@
+import pickle
+import tqdm
+from collections import Counter #???
+
+class TorchVocab(object): # ???
+    def __init__(self, counter, max_size=None, min_freq=1, specials=['<pad>', '<oov>'], 
+                vectors=None, unk_init=None, vectors_cache=None):
+        # counter: 글자수 세기
+        self.freqs = counter
+        counter = counter.copy()
+        min_freq = max(min_freq, 1)
+
+        self.itos = list(specials)
+
+        # special token을 counter에서 지워 카운트하지 않기
+        for tok in specials:
+            del counter[tok]
+        
+        max_size = None if max_size is None else max_size + len(self.itos)
+
+        # counter의 단어들을 빈도수에 따라 정렬
+        words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+
+        for word, freq in words_and_frequencies:
+            if freq < min_freq or len(self.itos) == max_size:
+                break
+            self.itos.append(word)
+        
+        # list self.itos를 딕셔너리형태로 변환
+        self.stoi = {tok: i for i, tok in enumerate(self.itos)}
+
+        self.vectors = None
+        if vectors is not None:
+            # ???
+            self.load_vectors(vectors, unk_init=unk_init, cache=vectors_cache)
+        else:
+            # assert: 뒤의 조건문이 True가 아니면 AssertError발생
+            assert unk_init is None and vectors_cache is None
+
+    def __eq__(self, other):
+        if self.freqs != other.freqs:
+            return False
+        if self.stoi != other.stoi:
+            return False
+        if self.itos != other.itos:
+            return False
+        if self.vectors != other.vectors:
+            return False
+        return True
+
+    def __len__(self):
+        return len(self.itos)
+
+    def vocab_rerank(self):
+        self.stoi = {word: i for i, word in enumerate(self.itos)}
+
+    def extend(self, v, sort=False):
+        words = sorted(v.itos) if sort else v.itos
+        for w in words:
+            if w not in self.stoi:
+                self.itos.append(w)
+                self.stoi[w] = len(self.itos) - 1
+
+
+class Vocab(TorchVocab):
+    def __init__(self, counter, max_size=None, min_freq=1):
+        self.pad_index = 0
+        self.unk_index = 1
+        self.eos_index = 2
+        self.sos_index = 3
+        self.mask_index = 4
+        
+        # 상속받은 부모 클래스
+        super().__init__(counter, specials=["<pad>", "<unk>", "<eos>", "<sos>", "<mask>"],
+                        max_size=max_size, min_freq=min_freq)
+
+        def to_seq(self, sentence, seq_len, with_eos=False, with_sos=False) -> list:
+            pass
+
+        def from_seq(self, seq, join=False, with_pad=False):
+            pass
+        
+        # vocab 불러오기 또는 저장
+        @staticmethod
+        def load_vocab(vocab_path: str) -> 'Vocab':
+            with open(vocab_path, "rb") as f:
+                return pickle.load(f)
+
+        def save_vocab(self, vocab_path):
+            with open(vocab_path, "wb") as f:
+                pickle.dump(self, f)
+
+
+class WordVocab(Vocab):
+    def __init__(self, texts, max_size=None, min_freq=1):
+        print("Building Vocab")
+        counter = Counter()
+
+        for line in tqdm.tqdm(texts):
+            # isinstance(a, list): a가 list형인지 판별
+            if isinstance(line, list):
+                words = line
+            else:
+                # 교체작업
+                words = line.replace("\n", "").replace("\t", "").split()
+
+            for word in words:
+                counter[word] += 1
+        super().__init__(counter, max_size=max_size, min_freq=min_freq)
+
+    def to_seq(self, sentence, seq_len=None, with_eos=False, with_sos=False, with_len=False):
+        if isinstance(sentence, str):
+            sentence = sentence.split()
+        
+        seq = [self.stoi.get(word, self.unk_index) for word in sentence]
+
+        if with_eos:
+            seq += [self.eos_index]
+        if with_sos:
+            seq = [self.sos_index] + seq
+
+        origin_seq_len = len(seq)
+
+        if seq_len is None:
+            pass
+        elif len(seq) <= seq_len:
+            seq += [self.pad_index for _ in range(seq_len - len(seq))]
+        else:
+            seq = seq[:seq_len]
+        
+        return (seq, origin_seq_len if with_len else seq)
+
+    def from_seq(self, seq, join=False, with_pad=False):
+        words = [self.itos[idx]
+                if idx < len(self.itos)
+                else "<%d>" % idx
+                for idx in seq
+                if not with_pad or idx != self.pad_index]
+
+        return " ".join(words) if join else words
+
+    @staticmethod
+    def load_vocab(vocab_path: str) -> 'WordVocab':
+        with open(vocab_path, "rb") as f:
+            return pickle.load(f)
+
+
+def build():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--corpus_path", require=True, type=str)
+    parser.add_argument("-o", "--output_path", required=True, type=str)
+    parser.add_argument("-s", "--vocab_size", type=int, default=None)
+    parser.add_argument("-e", "--encoding", type=str, default="utf-8")
+    parser.add_argument("-m", "--min_freq", type=int, default=1)
+    args = parser.parse_args()
+
+    with open(args.corpus_path, "r", encoding=args.encoding) as f:
+        vocab = WordVocab(f, max_size=args.vocab_size, min_freq=args.min_freq)
+
+    print("VOCAB SIZE:", len(vocab))
+    vocab.save_vocab(args.output_path)
