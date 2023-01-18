c74155f0a4166121e1c89c6d3d5cb62315f51c4b
CountingMstar <mwh510@naver.com>
2023-01-18 22:50:06 +0900

    bert practice

--- BERT-pytorch/model/embedding/position copy.py
+++ BERT-pytorch/model/embedding/position copy.py
@@ -7,15 +7,17 @@ class PositionalEmbedding(nn.Module):
         super().__init__()
 
         pe = torch.zeros(max_len, d_model).float()
-        pe.require_grad = False # ???
+        # require_grad: 역전파중 계산을 하지 않겠다. 즉 파라미터를 업데이트 하지 않겠다.
+        pe.require_grad = False 
 
-        # ???
         position = torch.arange(0, max_len).float().unsqueeze(1)
         div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0)/d_model)).exp()
 
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
 
+        # register_buffer: optimizer가 업데이트하지 않는다. 그러나 연산은 가능 
+        # 모델 매개 변수로 간주되지 않는 버퍼를 등록하는데 사용
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe) # ???
 
--- BERT-pytorch/model/embedding/position.py
+++ BERT-pytorch/model/embedding/position.py
@@ -10,6 +10,7 @@ class PositionalEmbedding(nn.Module):
 
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model).float()
+        # require_grad: 역전파중 계산을 하지 않겠다. 즉 파라미터를 업데이트 하지 않겠다.
         pe.require_grad = False
 
         position = torch.arange(0, max_len).float().unsqueeze(1)
@@ -19,6 +20,8 @@ class PositionalEmbedding(nn.Module):
         pe[:, 1::2] = torch.cos(position * div_term)
 
         pe = pe.unsqueeze(0)
+        # register_buffer: optimizer가 업데이트하지 않는다. 그러나 연산은 가능 
+        # 모델 매개 변수로 간주되지 않는 버퍼를 등록하는데 사용
         self.register_buffer('pe', pe)
 
     def forward(self, x):
